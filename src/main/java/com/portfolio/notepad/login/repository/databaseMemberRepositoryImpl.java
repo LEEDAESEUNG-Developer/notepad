0bbea038a95be3b6a6d96cebd558d7239c5cb3dc
LEEDAESEUNG-Developer <eotmd1016@gmail.com>
2022-06-27 07:36:26 +0900

    db 회원 기능(로그인, 회원가입) 추가
    db와 연결하여 로그인 회원가입 기능을 추가 하였음.

---
+++ src/main/java/com/portfolio/notepad/login/repository/databaseMemberRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.portfolio.notepad.login.repository;
+
+import com.portfolio.notepad.login.MemberEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Slf4j
+@Primary
+@Repository
+@RequiredArgsConstructor
+public class DatabaseMemberRepsotioryImpl implements MemberRepository {
+
+    private final MemberMapper memberMapper;
+
+    @Override
+    public MemberEntity findMember(MemberEntity member) {
+        return null;
+    }
+
+    @Override
+    public List<MemberEntity> getMembers() {
+        return memberMapper.getMemberAll();
+    }
+
+    @Override
+    public boolean isMember(MemberEntity member) { // LOGIN METHOD
+        log.info("isMember 함수 실행");
+        MemberEntity findMember = memberMapper.login(member);
+        log.info("findMember = {} ", findMember);
+
+        return findMember != null;
+    }
+
+    @Override
+    public void addMember(MemberEntity member) {
+
+    }
+}
